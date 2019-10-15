9696627e0c41692103097532540950fc792ae11d
m.stein <m.stein@conterra.de>
2019-10-15 10:29:02 +0200

    Reformat PopupDefinition.js

--- src/main/js/bundles/dn_relatedtables/PopupDefinition.js
+++ src/main/js/bundles/dn_relatedtables/PopupDefinition.js
@@ -25,17 +25,17 @@ export default class CustomPopupDefinition {
     }
 
     resolvePopupTemplate(layerOrSublayer) {
-        let that = this;
+        const that = this;
         let url = layerOrSublayer.url;
-        let layerId = layerOrSublayer.layerId;
+        const layerId = layerOrSublayer.layerId;
         if (layerId) {
             url = url + "/" + layerId;
         }
-        let queryController = this.queryController;
+        const queryController = this.queryController;
         return ct_when(queryController.getMetadata(url), (metadata) => {
             if (metadata.fields) {
-                let fields = metadata.fields;
-                let fieldNames = fields.map(f => f.name);
+                const fields = metadata.fields;
+                const fieldNames = fields.map(f => f.name);
                 let displayField = metadata.displayField;
                 let objectIdField = ct_array.arraySearchFirst(metadata.fields, {
                     type: "esriFieldTypeOID"
@@ -51,13 +51,13 @@ export default class CustomPopupDefinition {
                                 widget.startup();
                             }
 
-                            let sourceLayer = graphic.sourceLayer;
+                            const sourceLayer = graphic.sourceLayer;
                             displayField = displayField || sourceLayer.displayField;
                             objectIdField = objectIdField || sourceLayer.objectIdField;
-                            let objectId = graphic.attributes[objectIdField];
+                            const objectId = graphic.attributes[objectIdField];
 
-                            let title = graphic.attributes[displayField];
-                            let items = that.lookupFieldNamesToAttributes(fields, graphic.attributes);
+                            const title = graphic.attributes[displayField];
+                            const items = that.lookupFieldNamesToAttributes(fields, graphic.attributes);
                             widget.set("title", title);
                             widget.set("items", items);
                             widget.set("relatedRecordsTabs", []);
@@ -75,54 +75,50 @@ export default class CustomPopupDefinition {
 
     getRelatedRecordsTabs(sourceLayer, objectId, widget) {
         let url = sourceLayer.url;
-        let layerId = sourceLayer.layerId;
+        const layerId = sourceLayer.layerId;
         if (layerId) {
             url = url + "/" + layerId;
         }
         widget.set("loading", true);
 
-        let queryController = this.queryController;
-        return ct_when(queryController.getMetadata(url), (metadata) => {
-            return ct_when(queryController.getRelatedMetadata(url, metadata), (relatedMetadata) => {
-                return ct_when(queryController.findRelatedRecords(objectId, url, metadata), (results) => {
-                    let relatedRecordsTabs = [];
-                    if (!results) {
-                        widget.set("loading", false);
-                        return relatedRecordsTabs;
-                    }
-                    results.forEach((result, i) => {
-                        let tabs = [];
-                        let metadata = relatedMetadata[i];
-                        let fields = result.fields;
-                        let relatedRecordGroups = result.relatedRecordGroups;
-                        relatedRecordGroups.forEach((relatedRecordGroup) => {
-                            relatedRecordGroup.relatedRecords.forEach((record) => {
-                                let attributes = record.attributes;
-                                let items = this.lookupFieldNamesToAttributes(fields, attributes);
-                                const objectIdField = this.getObjectIdField(metadata.fields);
-                                tabs.push({
-                                    id: metadata.id + "_" + attributes[objectIdField.name],
-                                    title: attributes[metadata.displayField],
-                                    items: items
-                                });
-                            });
-                        });
-                        relatedRecordsTabs.push({
-                            id: metadata.id,
-                            title: metadata.name,
-                            tabs: tabs,
-                            active: null
+        const queryController = this.queryController;
+        return ct_when(queryController.getMetadata(url), (metadata) => ct_when(queryController.getRelatedMetadata(url, metadata), (relatedMetadata) => ct_when(queryController.findRelatedRecords(objectId, url, metadata), (results) => {
+            const relatedRecordsTabs = [];
+            if (!results) {
+                widget.set("loading", false);
+                return relatedRecordsTabs;
+            }
+            results.forEach((result, i) => {
+                const tabs = [];
+                const metadata = relatedMetadata[i];
+                const fields = result.fields;
+                const relatedRecordGroups = result.relatedRecordGroups;
+                relatedRecordGroups.forEach((relatedRecordGroup) => {
+                    relatedRecordGroup.relatedRecords.forEach((record) => {
+                        const attributes = record.attributes;
+                        const items = this.lookupFieldNamesToAttributes(fields, attributes);
+                        const objectIdField = this.getObjectIdField(metadata.fields);
+                        tabs.push({
+                            id: metadata.id + "_" + attributes[objectIdField.name],
+                            title: attributes[metadata.displayField],
+                            items: items
                         });
                     });
-                    widget.set("loading", false);
-                    return relatedRecordsTabs;
                 });
-            })
-        });
+                relatedRecordsTabs.push({
+                    id: metadata.id,
+                    title: metadata.name,
+                    tabs: tabs,
+                    active: null
+                });
+            });
+            widget.set("loading", false);
+            return relatedRecordsTabs;
+        })));
     }
 
     lookupFieldNamesToAttributes(fields, attributes) {
-        let result = [];
+        const result = [];
         fields.forEach((field) => {
             if (!this.properties.hideFields.includes(field.name)) {
                 result.push({
@@ -135,13 +131,11 @@ export default class CustomPopupDefinition {
     }
 
     getObjectIdField(fields) {
-        return fields.find((field) => {
-            return field.type === "esriFieldTypeOID";
-        });
+        return fields.find((field) => field.type === "esriFieldTypeOID");
     }
 
     cleanupPopupTemplate(layer) {
-        let widget = layer._$popup_widget;
+        const widget = layer._$popup_widget;
         delete layer._$popup_widget;
         widget && widget.destroyRecursive();
     }
