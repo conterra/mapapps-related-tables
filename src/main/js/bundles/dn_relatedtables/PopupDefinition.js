f6359f798eabbbe16dd13b13356074f283f87dd9
Matthias Stein <m.stein@conterra.de>
2020-09-17 13:56:08 +0200

    Update to map.apps 4.9.1

--- src/main/js/bundles/dn_relatedtables/PopupDefinition.js
+++ src/main/js/bundles/dn_relatedtables/PopupDefinition.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import moment from "esri/moment";
+import moment from "moment";
 
 export default class CustomPopupDefinition {
 
